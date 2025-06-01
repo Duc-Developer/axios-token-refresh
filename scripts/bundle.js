d2ab7419ba00df7f45b1a5382d393c935b4e2413
David Chan <mrtranduc1994@outlook.com>
2025-06-01 13:12:50 +0700

    fix: update support cjs bundle

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,14 +1,28 @@
 const build = async () => {
-    const minifyResponses = await Bun.build({
+    // ESM build
+    const esmResponses = await Bun.build({
         entrypoints: ['src/index.ts'],
         outdir: './dist',
         format: 'esm',
         naming: "[dir]/[name].esm.[ext]",
         minify: true,
-        external: ['axios'], 
+        external: ['axios'],
     });
-    if (!minifyResponses.success) {
+    if (!esmResponses.success) {
         throw new AggregateError(esmResponses.logs, "Bundle .esm failed");
+    }
+
+    // CJS build
+    const cjsResponses = await Bun.build({
+        entrypoints: ['src/index.ts'],
+        outdir: './dist',
+        format: 'cjs',
+        naming: "[dir]/[name].cjs.[ext]",
+        minify: true,
+        external: ['axios'],
+    });
+    if (!cjsResponses.success) {
+        throw new AggregateError(cjsResponses.logs, "Bundle .cjs failed");
     }
 };
 
