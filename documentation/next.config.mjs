b1d8428983ca18759ab83311d03e86ff966152d6
David Chan <mrtranduc1994@outlook.com>
2025-10-01 19:32:44 +0700

    feat: update dom

--- documentation/next.config.mjs
+++ documentation/next.config.mjs
@@ -6,6 +6,33 @@ const nextConfig = {
         defaultLocale: 'en',
         localeDetection: false,
     },
+    images: {
+        domains: ['img.shields.io', 'github.com'],
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+            {
+                source: '/api/(.*)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-cache, no-store, must-revalidate',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
